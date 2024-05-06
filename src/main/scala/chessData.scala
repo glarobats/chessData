51a7bc78976ba1645e838aa09fd1f6237d7604a0
Ioannis Kondilis <glarobats@gmail.com>
2024-05-06 18:18:20 +0300

    Initial commit

---
+++ src/main/scala/chessData.scala
@@ -0,0 +1,10 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object chessData{
+  def main(args: Array[String]): Unit = {
+    // δημιουργία RDD από το αρχείο εισόδου
+    val inputFile = "input/games.csv"
+
+
+  }
+}
